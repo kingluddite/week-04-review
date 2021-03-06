0e4a1d7744f8b0c917191968d61ad0dea8b4236c
philip howley <howley.phil@gmail.com>
2021-03-05 23:11:15 -0500

    Add string

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
-// with splice think "CUT"
-// mutates the original
-const months = ['Jan', 'March', 'April', 'June'];
-months.splice(1, 0, 'Feb');
-// inserts at index 1
-console.log(months);
-// expected output: Array ["Jan", "Feb", "March", "April", "June"]
+// with slice think "COPY"
+// Slice DOES NOT mutate the original array
+// DOCS - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
 
-months.splice(4, 1, 'May');
-// replaces 1 element at index 4
-console.log(months);
-// expected output: Array ["Jan", "Feb", "March", "April", "May"]
+console.log(animals.slice(2));
+// we remove items before array index 2 (ant and bison)
+// expected output: Array ["camel", "duck", "elephant"]
+
+console.log(animals.slice(2, 4));
+// we start at index 2 and end at index 4
+// note: slice extracts up to but not including end
+// expected output: Array ["camel", "duck"]
+
+console.log(animals.slice(1, 5));
+// expected output: Array ["bison", "camel", "duck", "elephant"]
+
+// we see that a new array is created
+const newAnimalsArr = animals.slice(1, 5);
+console.log(newAnimalsArr);
