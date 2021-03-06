05a460ea97190e25b99dc224262d1c7912d9163a
philip howley <howley.phil@gmail.com>
2021-03-05 23:27:50 -0500

    Add string really

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,15 @@
-// with slice think "COPY"
-// Slice DOES NOT mutate the original array
-// DOCS - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
-const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+// strings are not arrays
+// But in some ways they are similar to arrays
+// you can access each character in a string by it's index
+let str = 'icanloopthroughanarray';
+for (let i = 0; i < str.length; i++) {
+  console.log(str[i]);
+}
+/* view the console to see this! */
 
-console.log(animals.slice(2));
-// we remove items before array index 2 (ant and bison)
-// expected output: Array ["camel", "duck", "elephant"]
+// Change a letter
+let dogStr = 'dog';
+dogStr[0] = 'l';
 
-console.log(animals.slice(2, 4));
-// we start at index 2 and end at index 4
-// note: slice extracts up to but not including end
-// expected output: Array ["camel", "duck"]
-
-console.log(animals.slice(1, 5));
-// expected output: Array ["bison", "camel", "duck", "elephant"]
-
-// we see that a new array is created
-const newAnimalsArr = animals.slice(1, 5);
-console.log(newAnimalsArr);
+console.log(dogStr);
+// why not log? The reason is that strings are immutable
