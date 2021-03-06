517abf5cd46c61af45821ef1fc85d175e168d14f
philip howley <howley.phil@gmail.com>
2021-03-05 22:20:08 -0500

    Initialize repo

---
+++ README.md
@@ -0,0 +1,12 @@
+# Week 4 DOM review
+
+- For Loops
+- For ... Of Loops
+- Break & Continue
+- More Array Methods:
+  - concat()
+  - sort()
+  - reverse()
+  - splice()
+  - reverse()
+- Loop through string
---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let str = '';
+
+for (let i = 0; i < 9; i++) {
+  str = str + i;
+}
+
+console.log(str);
+// visualize tool - http://www.pythontutor.com/visualize.html#mode=edit
+// expected output: "012345678"
