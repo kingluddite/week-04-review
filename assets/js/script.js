a89b337fce04cbb1dbe9a6b160b4e08ab7864806
philip howley <howley.phil@gmail.com>
2021-03-05 22:36:47 -0500

    Add concat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,9 @@
-// when i is equal to 4, it skips over that iteration and console.log(i)
-//   doesn't run again until the next time that the code loops
-for (let i = 1; i <= 5; i++) {
-  if (i === 4) {
-    continue;
-  }
-  console.log(i);
-}
+// The concat() method is used to merge two or more arrays
+// This method does not change the existing arrays, but instead returns a new array.
+// source - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2);
 
-/* output */
-
-/*
-1
-2 
-3
-5
-*/
+console.log(array3);
+// expected output: Array ["a", "b", "c", "d", "e", "f"]
