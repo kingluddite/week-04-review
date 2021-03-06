0a8509a5aa9290b179eeea1f99ba6ef103c55dba
philip howley <howley.phil@gmail.com>
2021-03-06 00:06:25 -0500

    Add remove node

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,5 @@
-// grab the div with a class of container
-const containerEl = document.querySelector('.container');
-const containerTwoEl = document.querySelector('.container-two');
-// create a h1 element
-const h1El = document.createElement('h1');
-// create text
-const h1ElText = document.createTextNode('Hello World!');
-// add text to the h1 element
-h1El.appendChild(h1ElText);
-// add the h1 to the div element
-containerEl.appendChild(h1El);
-containerTwoEl.insertBefore();
-
-// how to use insertBefore() 
-https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_node_insertbefore
+const myParaEl = document.querySelector('#my-par');
+// this will remove the entire `<p>` and it's child `<img>`
+// myParaEl.remove();
+// this just removes the `<img>`
+// myParaEl.removeChild(myParaEl.firstChild);
