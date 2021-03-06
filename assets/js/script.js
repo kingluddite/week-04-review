73d4a6c141335d6b0d855af09076ab94a7bdabae
philip howley <howley.phil@gmail.com>
2021-03-06 00:12:03 -0500

    Add while remove nodes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,11 @@ const myParaEl = document.querySelector('#my-par');
 // myParaEl.remove();
 // this just removes the `<img>`
 // myParaEl.removeChild(myParaEl.firstChild);
+
+// while loop to remove
+// all the child nodes of a parent node (without removing the parent node)
+// const bodyEl = document.querySelector('body');
+// while (bodyEl.firstChild) {
+//   bodyEl.removeChild(bodyEl.firstChild);
+// }
+// Review slide for "Removing Nodes"
