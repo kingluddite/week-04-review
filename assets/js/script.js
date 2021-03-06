f2be3bad0431c8e55d265dccffb268df3c6524ef
philip howley <howley.phil@gmail.com>
2021-03-05 23:01:59 -0500

    Add splice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-const array1 = ['one', 'two', 'three'];
-console.log('array1:', array1);
-// expected output: "array1:" Array ["one", "two", "three"]
+// with splice think "CUT"
+// mutates the original
+const months = ['Jan', 'March', 'April', 'June'];
+months.splice(1, 0, 'Feb');
+// inserts at index 1
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "June"]
 
-const reversed = array1.reverse();
-console.log('reversed:', reversed);
-// expected output: "reversed:" Array ["three", "two", "one"]
-
-// Careful: reverse is destructive -- it changes the original array.
-console.log('array1:', array1);
-// expected output: "array1:" Array ["three", "two", "one"]
+months.splice(4, 1, 'May');
+// replaces 1 element at index 4
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "May"]
