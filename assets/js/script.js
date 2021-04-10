79ca3f961725ec8eaab2151f96d08dac8de5e3f5
philip howley <howley.phil@gmail.com>
2021-04-10 09:38:46 -0400

    Make a funny

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // the Break Statement terminated the loop right when the condition was met
-for (let i = 1; i <= 5; i++) {
-  if (i === 4) {
+for (let i = 1; i <= 15; i++) {
+  if (i === 14) {
     break;
   }
   console.log(i);
